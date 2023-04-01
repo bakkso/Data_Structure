8f624c57ed88e2a62b961b84bf4c70ae8003c62b
bakkso <bbakksso@gmail.com>
2023-04-01 14:18:15 +0900

    comment

--- Data_Structure/week4/3.7.py
+++ Data_Structure/week4/3.7.py
@@ -2,7 +2,7 @@ def same(A,B):
     for i in A:
         if i in B:
             return True
-    return False
+    return False #return이 한 번 더 들여쓰지 않도록 주의 (들여쓰기를 한 번 더 하면 다른 실행결과가 됨)
 
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
